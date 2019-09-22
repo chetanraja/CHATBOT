4a2ccf05aa3b401b106597386f50ad9740926011
chetanraja <47027984+chetanraja@users.noreply.github.com>
2019-09-22 13:06:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from chatterbot.trainers import ListTrainer
 import os
 bot=ChatBot('Bot')
 bot.set_trainer(ListTrainer)
-for files in os.listdir('C:/Users/computer/Downloads\chatterbot-corpus-master/chatterbot_corpus/data/english/'):
-   data=open('C:/Users/computer/Downloads\chatterbot-corpus-master/chatterbot_corpus/data/english/'+files,'r').readlines()
+for files in os.listdir('greetings.yml'):
+   data=open('greetings.yml'+files,'r').readlines()
    bot.train(data)
 words = ['fuck','fuckoff','masturbate' ]
 a=(" profanity present in the sentence")
