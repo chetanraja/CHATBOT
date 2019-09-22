60966ad88c98ceb89eb7198b102f7e176f0f5e66
chetanraja <47027984+chetanraja@users.noreply.github.com>
2019-09-22 13:46:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,11 +42,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Communications :: Chat',
         'Topic :: Internet',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7.3',
     ],
     test_suite='tests'
